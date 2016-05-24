f66465a93399489f75876b7e1373573b0cde6428
apple <sj@sj.local>
2016-05-24 12:25:45 +0800

    Initial Commit

---
+++ UsePickerViewToChoseData/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UsePickerViewToChoseData
+//
+//  Created by Sj on 16/5/24.
+//  Copyright © 2016年 SJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UsePickerViewToChoseData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UsePickerViewToChoseData
+//
+//  Created by Sj on 16/5/24.
+//  Copyright © 2016年 SJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
